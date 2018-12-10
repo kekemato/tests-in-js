77ad3dc81cc25c67fcd44b232fdbd735fec284e2
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-12-10 12:31:12 +0100

    first half of a day

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+export const orderTotal = (arrayOfOrders) => {
+    let sum = arrayOfOrders.reduce(
+        (reduced, item, index, array) => (
+            reduced + ((item.quantity !== undefined) ?
+                item.price * item.quantity
+                : item.price)
+        ), 0
+    )
+    const shipping = arrayOfOrders.find(item => item.shipping)
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    return (
+        shipping &&
+            sum >= (shipping.freeShipping + shipping.price) ?
+                sum - shipping.price
+                :
+                sum
+    )
+}
---
+++ src/index.test.js
@@ -0,0 +1,105 @@
+import { orderTotal } from './index'
+
+describe('Sum without quantity', () => {
+    const orders = [
+        {
+            name: 'lager',
+            price: 10,
+        },
+        {
+            name: 'APA',
+            price: 15
+        }
+    ]
+
+    test('Happy path failed', () => {
+        expect(
+            orderTotal(orders)
+        ).toBe(25)
+    })
+
+    test('Empty order list must equal 0!', () => {
+        expect(
+            orderTotal([])
+        ).toBe(0)
+    })
+})
+
+describe('Sum with quantity', () => {
+
+    const ordersWithQuantity = [
+        {
+            name: 'Lager',
+            price: 10,
+            quantity: 2
+        },
+        {
+            name: 'APA',
+            price: 15,
+            quantity: 1
+        }
+    ]
+    const ordersWithZeroQuantity = [
+        {
+            name: 'Lager',
+            price: 10,
+            quantity: 2
+        },
+        {
+            name: 'APA',
+            price: 15,
+            quantity: 0
+        }
+    ]
+
+    test('Fails to calculate sum with quantity', () => {
+        expect(
+            orderTotal(ordersWithQuantity)
+        ).toBe(35)
+    })
+
+
+    test('Fails to calculate sum with ZERO quantity', () => {
+        expect(
+            orderTotal(ordersWithZeroQuantity)
+        ).toBe(20)
+    })
+})
+
+describe('Sum with shipping', () => {
+    const orders = [
+        {
+            name: 'Lager',
+            price: 10,
+            quantity: 100,
+        },
+        {
+            shipping: true,
+            price: 25,
+            freeShipping: 200
+        }
+    ]
+
+    const orders2 = [
+        {
+            name: 'Lager',
+            price: 10,
+            quantity: 2,
+        },
+        {
+            shipping: true,
+            price: 25,
+            freeShipping: 200
+        }
+    ]
+
+    test('Free shipping >= 200', () => {
+        expect(orderTotal(orders)
+        ).toBe(1000)
+    })
+
+    test('Free shipping <= 200', () => {
+        expect(orderTotal(orders2)
+        ).toBe(45)
+    })
+})
